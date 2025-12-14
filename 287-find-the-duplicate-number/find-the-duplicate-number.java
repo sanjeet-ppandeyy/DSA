cd315d2be355ab3d89b8a2378e609a4615449006
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2025-12-14 12:38:01 +0530

    Time: 7 ms (40.53%) | Memory: 83 MB (26.91%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,26 +1,18 @@
 class Solution {
     public int findDuplicate(int[] arr) {
+        int n = arr.length;
         int i = 0;
-
-        while (i < arr.length) {
-            int correctIdx = arr[i] - 1;
-
-            if (arr[i] != arr[correctIdx]) {
-                swap(arr, i, correctIdx);
-            } else {
-                i++;
-            }
+        while(i < n){
+            int rightIdx = arr[i] - 1;
+            if(arr[i] == i+1 || arr[rightIdx] == arr[i]) i++;
+            else swap(arr,i,rightIdx);
         }
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i] != i + 1) {
-                return arr[i];
-            }
+        for(i=0; i<n; i++){
+            if(arr[i] != i+1) return arr[i];
         }
-
         return -1;
     }
-
-    private void swap(int[] arr, int i, int j) {
+    public void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
