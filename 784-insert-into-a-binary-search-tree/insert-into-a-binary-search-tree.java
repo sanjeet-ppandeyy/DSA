dbe877231d84e09a8b15939ff72c6e7189300494
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-01-10 22:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (29.59%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,6 +1,6 @@
 class Solution {
-    /*public void attach(TreeNode root, int val){
-        if(root == null) return;
+    public void attach(TreeNode root, int val){
+        if(root == null) return; 
         if(root.val == val) return;
         if(root.left == null && root.right == null){
             TreeNode t = new TreeNode(val);
@@ -8,30 +8,18 @@ class Solution {
             else root.left = t;
             return;
         }
-        if(root.left == null && root.right != null){
-            TreeNode t = new TreeNode(val);
-            if(root.val < val ) root.right = t;
-            else root.left = t;
-            return;
-        }
-        if(root.val < val) attach(root.right,val);
-        else attach(root.left,val);
-    }
-    */
-    public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root == null)  return new TreeNode(val);
-        if(root.val > val) {
-            if(root.left == null && root.right == null){
-                root.left = new TreeNode(val);
-            }
-            else root.left =insertIntoBST(root.left,val);
-        }
+        if(root.val < val){
+            if(root.right == null) root.right = new TreeNode(val);
+            attach(root.right,val);
+        } 
         else{
-            if(root.left == null && root.right == null){
-                root.right = new TreeNode(val);
-            }
-            else root.right = insertIntoBST(root.right,val);
-        }
+            if(root.left == null) root.left = new TreeNode(val);
+            attach(root.left,val);
+        } 
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        attach(root,val);
         return root;
     }
 }
