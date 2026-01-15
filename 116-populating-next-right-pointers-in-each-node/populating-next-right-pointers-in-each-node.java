5dfa2796db3a1a77a262e941ec4a65a2f8155cc5
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-01-15 20:16:12 +0530

    Time: 3 ms (37.73%) | Memory: 46.3 MB (88.50%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,19 @@
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(q.size()>0){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                Node temp = q.remove();
+                if(i==size-1) temp.next = null;
+                else temp.next = q.peek(); 
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+            } 
+        }
+        return root;
+    }
+}
