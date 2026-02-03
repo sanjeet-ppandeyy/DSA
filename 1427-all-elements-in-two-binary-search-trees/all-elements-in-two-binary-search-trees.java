2cb178c40cedb7a6d568ed5a728c903efb49901f
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-02-03 10:45:47 +0530

    Time: 21 ms (20.97%) | Memory: 48.5 MB (83.05%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,74 @@
+class Solution {
+    public void merge(List<Integer> a, List<Integer> b, List<Integer> ans) {
+        int i = 0, j = 0;
+        while (i < a.size() && j < b.size()) {
+            if (a.get(i) <= b.get(j)) {
+                ans.add(a.get(i));
+                i++;
+            } else {
+                ans.add(b.get(j));
+                j++;
+            }
+        }
+        if (i == a.size()) {
+            while (j < b.size()) {
+                ans.add(b.get(j));
+                j++;
+            }
+        }
+        if (j == b.size()) {
+            while (i < a.size()) {
+                ans.add(a.get(i));
+                i++;
+            }
+        }
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> a = new ArrayList<>();
+        TreeNode curr = root1;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr)
+                    pred = pred.right;
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                if (pred.right == curr) {
+                    pred.right = null;
+                    a.add(curr.val);
+                    curr = curr.right;
+                }
+            } else {
+                a.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        List<Integer> b = new ArrayList<>();
+        curr = root2;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr)
+                    pred = pred.right;
+                if (pred.right == null) {
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                if (pred.right == curr) {
+                    pred.right = null;
+                    b.add(curr.val);
+                    curr = curr.right;
+                }
+            } else {
+                b.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        merge(a, b, ans);
+        return ans;
+    }
+}
