5242f1ad10ad6cafbfd23972be501c125bd7e718
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-02-08 15:23:10 +0530

    Time: 1 ms (85.21%) | Memory: 44.1 MB (18.99%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -1,16 +1,22 @@
 class Solution {
-    int totalNode;
-    public int size(TreeNode root){
-        if(root == null) return 0;
-        return 1 + size(root.left) + size(root.right);
-    } 
-    public boolean isCBT(TreeNode root,int idx){
-        if(root == null) return true;
-        if(idx > totalNode) return false;
-        return isCBT(root.left,2*idx) && isCBT(root.right,2*idx + 1);
-    }
     public boolean isCompleteTree(TreeNode root) {
-        totalNode = size(root);
-       return isCBT(root,1);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        boolean CBT = false;
+
+        while (q.size() > 0) {
+            TreeNode curr = q.poll();
+
+            if (curr == null) {
+                CBT = true;
+            } else {
+                if (CBT) return false;
+                
+                q.add(curr.left);
+                q.add(curr.right);
+            }
+        }
+        return true;
     }
 }
