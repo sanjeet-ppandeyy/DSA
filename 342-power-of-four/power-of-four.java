8625622690ab9dc50d9f47afa8dc4e4a71956571
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-09 00:11:22 +0530

    Time: 1 ms (89.68%) | Memory: 42.5 MB (70.80%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,10 @@
 class Solution {
-    public boolean isPerfectSqrt(int n) {
-        int root = (int)(Math.sqrt(n));
-        return (root*root == n);
-    }
+    // public boolean isPerfectSqrt(int n) {
+    //     int root = (int)(Math.sqrt(n));
+    //     return (root*root == n);
+    // }
     public boolean isPowerOfFour(int n) {
         if(n<=0) return false;
-        return ((n & (n-1)) == 0)  && isPerfectSqrt(n);
+        return ((n & (n-1)) == 0)  && (n%3 == 1);
     }
 }
