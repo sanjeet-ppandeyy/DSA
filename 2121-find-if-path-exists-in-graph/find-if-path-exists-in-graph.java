bccd6329f0aacb8e696431d704a931c81e00b1e0
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-23 21:06:09 +0530

    Time: 161 ms (28.58%) | Memory: 279.5 MB (67.06%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,35 +1,31 @@
 class Solution {
-    public void bfs(int start, List<List<Integer>> adj, boolean[] vis,int end){
+    public void dfs(int start, List<List<Integer>> adj, boolean[] vis, int end) {
         vis[start] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        while(q.size() > 0){
-            int top = q.remove();
-            for(int i : adj.get(top)){
-                if(!vis[i]){
-                    q.add(i);
-                    vis[i] = true;
-                    if(i == end) return;
-                }
+        for (int i : adj.get(start)) {
+            if (!vis[i]) {
+                dfs(i,adj,vis,end);
+                if (i == end) return;
             }
         }
     }
+
     public boolean validPath(int n, int[][] edges, int start, int end) {
-        if(start == end) return true; 
+        if (start == end)
+            return true;
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             List<Integer> list = new ArrayList<>();
             adj.add(list);
-        } 
-        for(int i = 0; i < edges.length; i++){
+        }
+        for (int i = 0; i < edges.length; i++) {
             int a = edges[i][0];
             int b = edges[i][1];
 
-            adj.get(a).add(b);   
-            adj.get(b).add(a);   
+            adj.get(a).add(b);
+            adj.get(b).add(a);
         }
         boolean[] vis = new boolean[n];
-        bfs(start,adj,vis,end);
+        dfs(start, adj, vis, end);
         return vis[end];
     }
 }
