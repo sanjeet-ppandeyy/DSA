221836ee705143f5bc3567e8445c47ba3b998c84
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-23 20:28:02 +0530

    Time: 1 ms (85.76%) | Memory: 46.3 MB (77.83%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,24 +1,15 @@
 class Solution {
-    public void bfs(boolean[] visited,List<List<Integer>> rooms){
-        int n = rooms.size();
-        visited[0] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        while(q.size()>0){
-            int top = q.remove();
-            List<Integer> keys = rooms.get(top);
-            for(int j : keys){
-                if(!visited[j]){
-                    q.add(j);
-                    visited[j] = true;
-                }
-            }
+    public void dfs(int start, boolean[] visited, List<List<Integer>> rooms) {
+        visited[start] = true;
+        for (int j : rooms.get(start)) {
+            if (!visited[j]) dfs(j,visited,rooms);
         }
     }
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] visited = new boolean[n];
-        bfs(visited,rooms);
+        dfs(0,visited,rooms);
         for(int i=0; i<n; i++){
             if(!visited[i]) return false;
         }
