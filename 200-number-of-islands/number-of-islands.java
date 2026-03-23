e0252d9865514ecd22ae7259d89dbd6b674c9dd9
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-23 20:12:53 +0530

    Time: 3 ms (86.78%) | Memory: 52.4 MB (42.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,3 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col){
-        this.row = row;
-        this.col = col;
-    }
-}
 class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
@@ -15,7 +7,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1' && !vis[i][j]) {
-                    bfs(i, j, grid, vis);
+                    dfs(i, j, grid, vis);
                     count++;
                 }
             }
@@ -23,43 +15,21 @@ class Solution {
         return count;
     }
 
-    private void bfs(int i, int j, char[][] grid, boolean[][] vis) {
+    private void dfs(int i, int j, char[][] grid, boolean[][] vis) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
-        while (!q.isEmpty()) {
-            Pair top = q.remove();
-            int row = top.row;
-            int col = top.col;
-            //top row-1,col
-            if (row > 0) {
-                if (grid[row - 1][col] == '1' && !vis[row - 1][col]) {
-                    q.add(new Pair(row - 1, col));
-                    vis[row - 1][col] = true;
-                }
-            }
-            //bottom row+1,col
-            if (row + 1 < m) {
-                if (grid[row + 1][col] == '1' && !vis[row + 1][col]) {
-                    q.add(new Pair(row + 1, col));
-                    vis[row + 1][col] = true;
-                }
-            }
-            //left row,col-1
-            if (col > 0) {
-                if (grid[row][col - 1] == '1' && !vis[row][col - 1]) {
-                    q.add(new Pair(row, col - 1));
-                    vis[row][col - 1] = true;
-                }
-            }
-            //right row,col+1
-            if (col + 1 < n) {
-                if (grid[row][col + 1] == '1' && !vis[row][col + 1]) {
-                    q.add(new Pair(row, col + 1));
-                    vis[row][col + 1] = true;
-                }
-            }
-        }
+        vis[i][j] = true;
+        //top row-1,col
+        if (i-1 >= 0 && grid[i - 1][j] == '1' && !vis[i- 1][j]) 
+            dfs(i-1,j,grid,vis);
+        //bottom row+1,col
+        if (i+1 <= m-1 && grid[i + 1][j] == '1' && !vis[i + 1][j])
+            dfs(i+1,j,grid,vis);
+        //left row,col-1
+        if (j-1 >= 0 && grid[i][j - 1] == '1' && !vis[i][j - 1])
+            dfs(i,j-1,grid,vis);
+        //right row,col+1
+        if (j+1 <= n-1 && grid[i][j + 1] == '1' && !vis[i][j+ 1])
+            dfs(i,j+1,grid,vis);
     }
 }
