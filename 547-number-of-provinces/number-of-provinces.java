fc8b601ca6e9ae45324074cbe39b6f6dc751d26f
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-23 19:46:44 +0530

    Time: 1 ms (87.63%) | Memory: 47.3 MB (68.48%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,16 +1,9 @@
 class Solution {
-    public void bfs(int i, boolean[] visit, int[][] isConnected){
-        int n = isConnected.length;
+    public void dfs(int i, boolean[] visit, int[][] isConnected) {
         visit[i] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size()>0){
-            int top = q.remove();
-            for(int j=0; j<n; j++){
-                if(isConnected[top][j] == 1 && !visit[j]){
-                    q.add(j);
-                    visit[j] = true;
-                }
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visit[j]) {
+                dfs(j,visit,isConnected);
             }
         }
     }
@@ -18,9 +11,9 @@ class Solution {
         int n = isConnected.length;
         boolean[] visit = new boolean[n];
         int count = 0;
-        for(int i=0; i<n; i++){
-            if(!visit[i]){
-                bfs(i,visit,isConnected);
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                dfs(i, visit, isConnected);
                 count++;
             }
         }
