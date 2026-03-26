b06ae567e2d54926a6ddd46ac115e7cfffe4584d
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-03-26 17:26:05 +0530

    Time: 11 ms (68.66%) | Memory: 48.8 MB (86.09%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,52 @@
+class Pair implements Comparable<Pair>{
+    int node;
+    int dis;
+    Pair(int node, int dis){
+        this.node = node;
+        this.dis = dis;
+    }
+    public int compareTo(Pair d){
+        return Integer.compare(this.dis,d.dis);
+    }
+}
+
+class Solution {
+    public int networkDelayTime(int[][] edges, int n, int k){
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0; i<=n; i++) adj.add(new ArrayList<>());
+        for(int i = 0; i < edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+
+            adj.get(u).add(new Pair(v, w));
+        }
+        int[] ans = new int[n + 1];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[k] = 0;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(k,0));
+        while (pq.size() > 0){
+            Pair top = pq.remove();
+            int node = top.node;
+            int dis = top.dis;
+            for (Pair p : adj.get(node)){
+                int adjNode = p.node;
+                int weight = p.dis;
+
+                if(dis + weight < ans[adjNode]){
+                    ans[adjNode] = dis + weight;
+                    pq.add(new Pair(adjNode,ans[adjNode]));
+                }
+            }
+        }
+        int res = -1;
+        for (int i=1; i<=n; i++){
+            if(ans[i] == Integer.MAX_VALUE) return -1;
+            if(ans[i] > res) res = ans[i];
+        }
+        return res;
+    }
+}
+
