28d58d51c0c78900a97e114a65894dd6bf9e5712
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-04-04 00:48:43 +0530

    Time: 570 ms (25.90%) | Memory: 76.8 MB (63.03%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Triplet implements Comparable<Triplet> {
+    int node;
+    int parent;
+    int cost;
+
+    Triplet(int node, int parent, int cost) {
+        this.node = node;
+        this.parent = parent;
+        this.cost = cost;
+    }
+
+    public int compareTo(Triplet t) {
+        return this.cost - t.cost;
+    }
+}
+
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int sum = 0;
+        boolean[] vis = new boolean[points.length];
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(0,-1,0));
+        while (pq.size()>0){
+            Triplet top = pq.remove();
+            int node = top.node;
+            int cost = top.cost;
+
+            if (vis[node]) continue;
+            vis[node] = true;
+            sum += cost;
+
+            for(int i=0; i<points.length; i++){
+                int x1 = points[node][0], y1 = points[node][1];
+                int x2 = points[i][0], y2 = points[i][1];
+                int mDis = Math.abs(x2-x1) + Math.abs(y2-y1);
+                if(!vis[i]) pq.add(new Triplet(i,node,mDis));
+            }
+        }
+        return sum;
+    }
+}
