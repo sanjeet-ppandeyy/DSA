d132fe91cf452587b1077f790e87d321cfaccfc0
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-04-30 01:22:15 +0530

    Time: 13 ms (47.51%) | Memory: 48.8 MB (74.12%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,53 +1,22 @@
-class Pair implements Comparable<Pair>{
-    int node;
-    int dis;
-    Pair(int node, int dis){
-        this.node = node;
-        this.dis = dis;
-    }
-    public int compareTo(Pair d){
-        return Integer.compare(this.dis,d.dis);
-    }
-}
-
 class Solution {
-    public int networkDelayTime(int[][] edges, int n, int k){
-        List<List<Pair>> adj = new ArrayList<>();
-        for(int i=0; i<=n; i++) adj.add(new ArrayList<>());
-        for(int i = 0; i < edges.length; i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-
-            adj.get(u).add(new Pair(v, w));
-        }
-        int[] ans = new int[n + 1];
-        Arrays.fill(ans, Integer.MAX_VALUE);
-        ans[k] = 0;
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(k,0));
-        while (pq.size() > 0){
-            Pair top = pq.remove();
-            int node = top.node;
-            int dis = top.dis;
-            if(dis > ans[node]) continue;
-            for (Pair p : adj.get(node)){
-                int adjNode = p.node;
-                int weight = p.dis;
-
-                if(dis + weight < ans[adjNode]){
-                    ans[adjNode] = dis + weight;
-                    pq.add(new Pair(adjNode,ans[adjNode]));
-                }
+    public int networkDelayTime(int[][] times, int n, int src) {
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src] = 0;
+        for(int x=1; x<n; x++){
+            for(int i=0; i<times.length; i++){
+                int u = times[i][0];
+                int v = times[i][1];
+                int w = times[i][2];
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v])
+                    dist[v] = dist[u] + w; 
             }
         }
-        int res = -1;
-        for (int i=1; i<=n; i++){
-            if(ans[i] == Integer.MAX_VALUE) return -1;
-            if(ans[i] > res) res = ans[i];
+        int max = 0;
+        for(int i=1; i<=n; i++){
+            if(dist[i] == Integer.MAX_VALUE) return -1;
+            max = Math.max(max,dist[i]);
         }
-        return res;
+        return max;
     }
 }
-
