8e4e3b47522bca14c1fd3a8a4d926f87470967e8
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-05-02 00:27:11 +0530

    Time: 11 ms (44.75%) | Memory: 46.7 MB (23.23%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i=0; i<edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+        for (int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                if(i==k) continue;
+                for(int j=0; j<n; j++){
+                    if(j==k) continue;
+                    if(dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE)
+                        dist[i][j] = Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        int minCity = -1;
+        int minCount = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++){
+            int count = 0;
+            for (int j=0; j<n; j++){
+                if (i==j) continue;
+                if (dist[i][j] <= distanceThreshold) count++;
+            }
+            if(count <= minCount){
+                minCount = count;
+                minCity = i;
+            }
+        }
+        return minCity;
+    }
+}
