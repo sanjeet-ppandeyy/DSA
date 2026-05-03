43070f3751fd36dda1d7a3ef8d009a735522c2e4
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-05-03 20:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (93.27%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        String c = s + s;
+        if(c.contains(goal)) return true;
+        return false;
+    }
+}
