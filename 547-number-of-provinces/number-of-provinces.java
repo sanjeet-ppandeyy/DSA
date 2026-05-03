9fc0f824e9071c923d1718a053d6bed2ce51e564
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-05-03 19:51:59 +0530

    Time: 7 ms (5.26%) | Memory: 47.1 MB (86.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,24 +1,33 @@
 import java.util.*;
 
 class Solution {
-    int[] parent;
+    static int[] parent;
+    static int[] size;
     private int find(int a) {
         if(parent[a] == a) return a;
         return find(parent[a]);
     }
     private void union(int a, int b) {
-        int leaderA = find(a);
-        int leaderB = find(b);
-        if(leaderA != leaderB){
-            parent[leaderB] = leaderA;
+        a = find(a);
+        b = find(b);
+        if(a != b){
+            if (size[a] > size[b]){
+                parent[b] = a;
+                size[a] += size[b];
+            }else {
+                parent[a] = b;
+                size[b] = size[a];
+            }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         parent = new int[n+1];
+        size = new int[n+1];
         for (int i=1; i<=n; i++){
             parent[i] = i;
+            size[i] = 1;
         }
         for (int i=0; i<n; i++){
             for (int j=0; j<n; j++){
