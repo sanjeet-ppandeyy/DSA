9d450336676d7764ad733533db34330eac525d8d
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-06-15 13:13:23 +0530

    Time: 47 ms (5.02%) | Memory: 61.6 MB (7.25%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -2,31 +2,19 @@ import java.util.*;
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int n = nums.length;
-        int i = 0, j = 0, a = 0, b = 0;
-        int k2 = 0;
+        for(int x=0; x<n; x++) nums[x] %= 2;
+        for(int x=1; x<n; x++) nums[x] += nums[x-1];
+        
         int count = 0;
-        for(int x=0; x<n; x++){
-            if(nums[x] % 2 == 0) nums[x] = 0;
-            else nums[x] = 1;
-        }
-        while (i<n && nums[i]==0){
-            i++;
-        }
-        while (j < n && k2 < k) if (nums[j++] == 1) k2++;
-        if(k2 < k) return 0;
-        j--;
-        b = j + 1;
-        while (b < n && nums[b] == 0) b++;
-        b--;
-        while (b < n){
-            count += (i-a+1) * (b-j+1);
-            a = i+1;
-            i++;
-            while (i<n && nums[i] == 0) i++;
-            j = b + 1;
-            b = j + 1;
-            while (b < n && nums[b] == 0) b++;
-            b--;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int x=0; x<n; x++){
+            if (!map.containsKey(nums[x])) map.put(nums[x],x);
+            int a = 0;
+            if(map.containsKey(nums[x]-k)) a = map.get(nums[x]-k);
+            int b = 0;
+            if(map.containsKey(nums[x]-k+1)) b = map.get(nums[x]-k+1);
+            if(nums[x] == k) count += b-a+1;
+            if(nums[x] > k) count += (b-a);
         }
         return count;
     }
