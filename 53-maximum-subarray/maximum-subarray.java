bfd42d540a58cc454010be52192e2bbdf5158bce
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-09-06 16:35:50 +0530

    Time: 19 ms (1.45%) | Memory: 92 MB (5.79%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,16 @@
 class Solution {
-    public int helper(int i,int sum, int maxSum, int[] nums){
+    public int helper(int i,int sum, int maxSum, int[] nums,int[] dp){
         if(i == nums.length) return maxSum;
         sum += nums[i];
         maxSum = Math.max(maxSum,sum);
         if(sum < 0) sum = 0;
-        return helper(i+1,sum,maxSum,nums);
+        if(dp[i] != Integer.MIN_VALUE) return dp[i];
+        return dp[i] = helper(i+1,sum,maxSum,nums,dp);
     }
     public int maxSubArray(int[] nums) {
-        return helper(0,0,Integer.MIN_VALUE,nums);
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+        return helper(0,0,Integer.MIN_VALUE,nums,dp);
     }
 }
