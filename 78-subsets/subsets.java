b1e4d6bde9d60b392d487e78c68c5c9d1f8138f0
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2026-09-17 14:24:40 +0530

    Time: 1 ms (87.24%) | Memory: 44.2 MB (38.94%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int n = nums.length;
-        int total = (1<<n);
-        for(int i=0; i<total; i++){
-            List<Integer> ll = new ArrayList<>();
-            for(int bitIdx=0; bitIdx<n; bitIdx++){
-                int mask = (1<<bitIdx);
-                if((i & mask) != 0) ll.add(nums[bitIdx]);
-            }
-            ans.add(ll);
+    List<List<Integer>> ans = new ArrayList<>();
+    public void helper(int i, int[] nums, List<Integer> list){
+        if(i == nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
         }
+        list.add(nums[i]);
+        helper(i+1,nums,list);
+        list.remove(list.size()-1);
+        helper(i+1,nums,list);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        helper(0,nums,new ArrayList<>());
         return ans;
     }
 }
